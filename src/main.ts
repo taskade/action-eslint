cecaae01ff679c62700569a4eef079f94bedf62d
Duncan Leo <hello@duncanleo.me>
2020-03-03 10:59:00 +0800

    chore: split annotations into chunks of 50

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,10 @@ import * as core from '@actions/core';
 import * as github from '@actions/github';
 import { CHECK_NAME, EXTENSIONS_TO_LINT } from './constants';
 import { eslint } from './eslint-cli';
-import {Octokit} from '@octokit/rest';
+import { Octokit } from '@octokit/rest';
 import { graphql } from '@octokit/graphql';
 import fs from 'fs';
+import { chunk } from 'lodash';
 
 /**
  * This is just for syntax highlighting, does nothing
@@ -22,7 +23,7 @@ async function run() {
   const graphqlWithAuth = graphql.defaults({
     headers: {
       authorization: `token ${core.getInput('repo-token', { required: true })}`,
-    }
+    },
   });
 
   const prInfo = await graphqlWithAuth(
@@ -49,8 +50,8 @@ async function run() {
     {
       owner: context.repo.owner,
       name: context.repo.repo,
-      prNumber: context.issue.number
-    }
+      prNumber: context.issue.number,
+    },
   );
   if (prInfo === null) {
     core.setFailed('Could not get PR info!');
@@ -67,8 +68,8 @@ async function run() {
   if (filesToLint.length < 1) {
     console.warn(
       `No files with [${[...EXTENSIONS_TO_LINT].join(
-        ', '
-      )}] extensions added or modified in this PR, nothing to lint...`
+        ', ',
+      )}] extensions added or modified in this PR, nothing to lint...`,
     );
     return;
   }
@@ -79,32 +80,37 @@ async function run() {
     const checks = await octokit.checks.listForRef({
       ...context.repo,
       status: 'in_progress',
-      ref: currentSha
+      ref: currentSha,
     });
-    const theCheck = checks.data.check_runs.find(
-      ({ name }) => name === givenCheckName
-    );
+    const theCheck = checks.data.check_runs.find(({ name }) => name === givenCheckName);
     if (theCheck) checkId = theCheck.id;
   }
   if (!checkId) {
-    checkId = (await octokit.checks.create({
-      ...context.repo,
-      name: CHECK_NAME,
-      head_sha: currentSha,
-      status: 'in_progress',
-      started_at: new Date().toISOString()
-    })).data.id;
+    checkId = (
+      await octokit.checks.create({
+        ...context.repo,
+        name: CHECK_NAME,
+        head_sha: currentSha,
+        status: 'in_progress',
+        started_at: new Date().toISOString(),
+      })
+    ).data.id;
   }
 
   try {
     const { conclusion, output } = await eslint(filesToLint);
-    await octokit.checks.update({
-      ...context.repo,
-      check_run_id: checkId,
-      completed_at: new Date().toISOString(),
-      conclusion,
-      output
-    });
+    const { annotations } = output;
+
+    // Split into chunks of 50
+    for (const annotationsChunk of chunk(annotations, 50)) {
+      await octokit.checks.update({
+        ...context.repo,
+        check_run_id: checkId,
+        completed_at: new Date().toISOString(),
+        conclusion,
+        output: Object.assign(output, { annotations: annotationsChunk }),
+      });
+    }
 
     if (conclusion === 'failure') {
       core.setFailed(`ESLint found some errors`);
@@ -114,7 +120,7 @@ async function run() {
       ...context.repo,
       check_run_id: checkId,
       conclusion: 'failure',
-      completed_at: new Date().toISOString()
+      completed_at: new Date().toISOString(),
     });
     core.setFailed(JSON.stringify(error));
   }
